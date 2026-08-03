ac9b2b48b247ab271cff47cfcc672ddbca3e7abe
Yerik Agustín Gonzáles García <116103479+Yer1kg@users.noreply.github.com>
2026-08-03 18:53:28 +0100

    Update Vite config to include build targets
    Added build and esbuild target configurations for ESNext.

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs.plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Esto fuerza a que todos los recursos partan de la raíz
-})
+  build: {
+    target: 'esnext',
+  },
+  esbuild: {
+    target: 'esnext',
+  },
+});
