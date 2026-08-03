378319fdd5df63cd43a04ad4f664a83fda139f07
Yerik Agustín Gonzáles García <116103479+Yer1kg@users.noreply.github.com>
2026-08-03 18:57:24 +0100

    Fix import statement for @vitejs/plugin-react

--- packages/server/vitest.config.js
+++ packages/server/vitest.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs.plugin-react';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext', // <-- ESTA LÍNEA ES LA CLAVE
+    target: 'esnext',
   },
   esbuild: {
-    target: 'esnext', // <-- Opcional, para forzar también a esbuild en dev/build
+    target: 'esnext',
   },
 });
