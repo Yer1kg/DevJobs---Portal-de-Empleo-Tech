28dcc552e2fe0c659224d2b9a6a348c13808907a
Yerik Agustín Gonzáles García <116103479+Yer1kg@users.noreply.github.com>
2026-08-04 16:54:36 +0100

    Update API_URL import path in Home.jsx

--- packages/frontend/src/pages/Home.jsx
+++ packages/frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { SearchBar } from '../components/SearchBar'
 import { JobCard } from '../components/JobCard'
-import { API_URL } from '../config/api';
+import { API_URL } from '../api';
 
 
 
